a025a5fcae7ee1c2636131e7eae1ff1f9ca952f8
legend80s <liuchzong@qq.com>
2024-08-19 09:24:43 +0800

    feat: initial commit

---
+++ index.d.ts
@@ -0,0 +1,20 @@
+interface INpmPublishOutput {
+  tarballContents: ITarballContent[];
+  tarballDetails: ITarballDetails;
+}
+
+interface ITarballDetails {
+  name: string;
+  version: string;
+  filename: string;
+  packageSize: string;
+  unpackedSize: string;
+  shasum: string;
+  integrity: string;
+  totalFiles: string;
+}
+
+interface ITarballContent {
+  file: string;
+  size: string;
+}
---
+++ index.mjs
@@ -0,0 +1,1 @@
+export { parseNpmOutputToJSON as toJSON } from './lib/index.mjs';
